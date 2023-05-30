f0a512eecbf88d6a65ad0c9f330faf94f1537af7
Santosh <vinayaksantu@gmail.com>
2023-05-31 01:46:09 +0530

    this is my first commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,30 @@
+package api.endpoints;
+
+/*
+ * Swagger URI---->           https://petstore.swagger.io
+    Create User	(Post)	: https://petstore.swagger.io/v2/user
+	Get User	(Get)	: https://petstore.swagger.io/v2/user/{username}
+	Update User	(PUT)	: https://petstore.swagger.io/v2/user/{username}
+	Delete User	(Delete): https://petstore.swagger.io/v2/user/{username}
+*/
+
+public class Routes {
+	
+	//User Module
+	 public static String base_url="https://petstore.swagger.io/v2";
+	 
+	 public static String post_url=base_url+"/user";
+	 public static String get_url=base_url+"/user/{username}";
+	 public static String put_url=base_url+"/user/{username}";
+	 public static String delete_url=base_url+"/user/{username}";
+	 
+	 //Store Module
+	 
+	     //Here you will create Store module url's
+	 //Pet Module
+	 
+	    //Here you will create Pet module url's
+	
+	
+	
+}
---
+++ src/test/java/api/test/UserTests2.java
@@ -0,0 +1,87 @@
+package api.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints;
+import api.endpoints.UserEndPoints2;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTests2 {
+
+	Faker faker;
+	User userPayload;
+	
+	public Logger logger; //for logs
+	
+	@BeforeClass
+	public void setup() {
+		faker=new Faker();
+		userPayload=new User();
+
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstname(faker.name().firstName());
+		userPayload.setLastname(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5, 10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		
+		//logs
+		logger=LogManager.getLogger(this.getClass());
+		logger.debug("debugging...");
+
+	}
+
+	@Test(priority=1)
+	public void testPostUser() {
+
+		logger.info("****************Creating User******************");
+		Response response=UserEndPoints2.createUser(userPayload);
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("****************User Created******************");
+	}
+
+	@Test(priority=2)
+	public void testGetUserByName() {
+		logger.info("****************Reading User Info******************");
+		Response response=UserEndPoints2.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("****************User Info Displayed******************");
+	}
+
+	@Test(priority=3)
+	public void testUpdateUserByName() {
+		logger.info("****************Updating User******************");
+		//Update data using payload
+		userPayload.setFirstname(faker.name().firstName());
+		userPayload.setLastname(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+
+		Response response=UserEndPoints2.updateUser(this.userPayload.getUsername(), userPayload);
+		response.then().log().body();
+		Assert.assertEquals(response.getStatusCode(), 200);
+
+		//Checking the data after update
+		Response responseAfterUpdate=UserEndPoints2.readUser(this.userPayload.getUsername());
+		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
+		logger.info("****************User Updated******************");
+	}
+
+	@Test(priority=4)
+	public void testDeleteUserByName() {
+		logger.info("****************Deleting User******************");
+		Response response=UserEndPoints2.deleteUser(this.userPayload.getUsername());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("****************User Deleted******************");
+	}
+
+}
